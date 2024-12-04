b2f844e8c77fd84dad88c0e26079955b3a54c475
imaryiem <1maryiem363@gmail.com>
2024-12-04 12:25:13 +0300

    Initial Commit

---
+++ Forsa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Forsa
+//
+//  Created by BP-36-201-14 on 04/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
